277235feefe02666286bb824879fd97da0348436
Oceanos1 <evondom@hotmail.com>
2018-01-10 18:11:21 +0100

    refactor3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,10 +4,10 @@ import util.Constantes;
 public class Main {
     public static void main(String[] args) {
         if(args.length != 1){
-            System.err.println("Passez l'id du site a démarré comme unique paramètre");
+            System.err.println("Passez l'id du site à démarrer comme unique paramètre");
         }else{
             int id = Integer.parseInt(args[0]);
-            Site site = new Site(id, Constantes.PORT_DEFAUT + id);
+            Site site = new Site(id, Constantes.PORTS[id]);
         }
 
     }
