5437449b2245f7609262918bebc9820518a4ef21
Edward Ransome <edward.ransome@heig-vd.ch>
2018-01-10 19:55:17 +0100

    aldo4

--- src/main/java/util/Constantes.java
+++ src/main/java/util/Constantes.java
@@ -2,16 +2,23 @@ package util;
 
 import java.net.InetAddress;
 
+/**
+ * Interface contenant les constantes de l'application permettant de les
+ * modifier facilement.
+ */
 public interface Constantes {
     int[] PORTS = {6060,6061,6062,6063};
     String[] ADRESSES_IP = {"127.0.0.1","127.0.0.1","127.0.0.1","127.0.0.1"};
     int NOMBRE_DE_SITES = ADRESSES_IP.length;
-    int TAILLE_TAMPON_ANNONCE = Byte.BYTES + NOMBRE_DE_SITES * Integer.BYTES ;//un octet pour le type de message + Nombre de site * taille d'un int pour connaitre la taille de la liste d'aptitude
-    int TAILLE_TAMPON_RESULTAT = Byte.BYTES + Integer.BYTES + NOMBRE_DE_SITES * Integer.BYTES ;//type de message + elu + liste des sites ayant vu le resultat
+    //un octet pour le type de message + Nombre de site * taille d'un int pour connaitre la taille de la liste d'aptitude
+    int TAILLE_TAMPON_ANNONCE = Byte.BYTES + NOMBRE_DE_SITES * Integer.BYTES ;
+    //type de message + elu + liste des sites ayant vu le resultat
+    int TAILLE_TAMPON_RESULTAT = Byte.BYTES + Integer.BYTES + NOMBRE_DE_SITES * Integer.BYTES ;
     int TAILLE_TAMPON = Math.max(TAILLE_TAMPON_ANNONCE,TAILLE_TAMPON_RESULTAT);
-    int TAILLE_TAMPON_QUITTANCE = Integer.BYTES; //Id de celui qui envoie la quittance pour signifier qu'il a bien recu la liste
+    //Id de celui qui envoie la quittance pour signifier qu'il a bien recu la liste
+    int TAILLE_TAMPON_QUITTANCE = Integer.BYTES;
     int MESSAGE_TIMEOUT = 200;
-    int ELECTION_TIMEOUT = MESSAGE_TIMEOUT* NOMBRE_DE_SITES; //T unités de temps
+    int ELECTION_TIMEOUT = MESSAGE_TIMEOUT * NOMBRE_DE_SITES; //T unités de temps
 
     // Types de messages
     byte ANNONCE = (byte) 0;
