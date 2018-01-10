a0d8672ed2b74285ba8dbec9effe60f5171eb526
Oceanos1 <evondom@hotmail.com>
2018-01-10 14:35:01 +0100

    a

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ import util.Constants;
 public class Main {
     public static void main(String[] args) {
         if(args.length != 1){
-            System.err.println("Passez l'id du site a démarrer comme unique paramètre");
+            System.err.println("Passez l'id du site a démarré comme unique paramètre");
         }else{
             int id = Integer.parseInt(args[0]);
             Site site = new Site(id, Constants.BASE_PORT + id);
--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -4,5 +4,5 @@ public class Constants {
     public static final int BASE_PORT = 6060;
     public static final int NUMBER_OF_SITES = 4;
     public static final int MESSAGE_TIMEOUT = 200;
-    public static final int ELECTION_TIMEOUT = 2*MESSAGE_TIMEOUT*NUMBER_OF_SITES;
+    public static final int ELECTION_TIMEOUT = MESSAGE_TIMEOUT*NUMBER_OF_SITES; //T unités de temps
 }
